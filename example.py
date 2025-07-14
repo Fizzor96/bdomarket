930f18e4e956f8099cc0e76e8c21e1daf7a9d5fd
Fizzor96 <marwel96@gmail.com>
2025-07-15 01:37:09 +0200

    Refactor example.py to enhance output handling and improve readability; added print statements for iterable content.

--- example.py
+++ example.py
@@ -7,15 +7,17 @@ market.GetWorldMarketWaitList().content
 market.GetWorldMarketWaitList().statuscode
 market.GetWorldMarketWaitList().success
 market.GetWorldMarketWaitList().message # this is kinda pointless, devs did not implement such functionality
-market.GetWorldMarketWaitList().GetIterableContent() # converts json to py iterable type
-market.GetWorldMarketWaitList().SaveToFile("path") # saving output to file
+iterable = market.GetWorldMarketWaitList().GetIterableContent() # Deserialize to a Python object.
+for item in iterable:
+    print(item)
+market.GetWorldMarketWaitList().SaveToFile("responses/waitlist/get.json") # saving output to file
 print(bdomarket.ConvertTimestamp(1745193600000)) # can be useful if you don't know how to convert Unix timestamps to human readable format. Note (Post)GetMarketPriceInfo return timestamps...
 
 # WaitList
 market.GetWorldMarketWaitList().SaveToFile("responses/waitlist/get.json")
 market.PostGetWorldMarketWaitList().SaveToFile("responses/waitlist/post.json")
-print(market.GetWorldMarketWaitList())
-print(market.PostGetWorldMarketWaitList())
+# print(market.GetWorldMarketWaitList())
+# print(market.PostGetWorldMarketWaitList())
 
 # HotList
 market.GetWorldMarketHotList().SaveToFile("responses/hotlist/get.json")
@@ -26,47 +28,47 @@ print(market.PostGetWorldMarketHotList())
 # List
 market.GetWorldMarketList("1", "1").SaveToFile("responses/list/get.json")
 market.PostGetWorldMarketList("1", "1").SaveToFile("responses/list/post.json")
-print(market.GetWorldMarketList("1", "1"))
-print(market.PostGetWorldMarketList("1", "1"))
+# print(market.GetWorldMarketList("1", "1"))
+# print(market.PostGetWorldMarketList("1", "1"))
 
 
 # SubList
 market.GetWorldMarketSubList(["735008", "731109"]).SaveToFile("responses/sublist/get.json")
 market.PostGetWorldMarketSubList(["735008", "731109"]).SaveToFile("responses/sublist/post.json")
-print(market.GetWorldMarketSubList(["735008", "731109"]))
-print(market.PostGetWorldMarketSubList(["735008", "731109"]))
+# print(market.GetWorldMarketSubList(["735008", "731109"]))
+# print(market.PostGetWorldMarketSubList(["735008", "731109"]))
 
 
 # SearchList
 market.GetWorldMarketSearchList(["735008", "731109"]).SaveToFile("responses/searchlist/get.json")
 market.PostGetWorldMarketSearchList(["735008", "731109"]).SaveToFile("responses/searchlist/post.json")
-print(market.GetWorldMarketSearchList(["735008", "731109"]))
-print(market.PostGetWorldMarketSearchList(["735008", "731109"]))
+# print(market.GetWorldMarketSearchList(["735008", "731109"]))
+# print(market.PostGetWorldMarketSearchList(["735008", "731109"]))
 
 
 # BiddingInfo
 market.GetBiddingInfo(["735008", "731109"], ["19", "20"]).SaveToFile("responses/bidding/get.json")
 market.PostGetBiddingInfo(["735008", "731109"], ["19", "20"]).SaveToFile("responses/bidding/post.json")
-print(market.GetBiddingInfo(["735008", "731109"], ["19", "20"]))
-print(market.PostGetBiddingInfo(["735008", "731109"], ["19", "20"]))
+# print(market.GetBiddingInfo(["735008", "731109"], ["19", "20"]))
+# print(market.PostGetBiddingInfo(["735008", "731109"], ["19", "20"]))
 
 
 # PriceInfo
 market.GetMarketPriceInfo(["735008", "731109"], ["19", "20"]).SaveToFile("responses/priceinfo/get.json")
 market.PostGetMarketPriceInfo(["735008"], ["20"]).SaveToFile("responses/priceinfo/post.json")
-print(market.GetMarketPriceInfo(["735008", "731109"], ["19", "20"]))
-print(market.PostGetMarketPriceInfo(["735008"], ["20"]))
+# print(market.GetMarketPriceInfo(["735008", "731109"], ["19", "20"]))
+# print(market.PostGetMarketPriceInfo(["735008"], ["20"]))
 
 
 # PearItems
 market.GetPearlItems().SaveToFile("responses/pearlitems/get.json")
 market.PostGetPearlItems().SaveToFile("responses/pearlitems/post.json")
-print(market.GetPearlItems())
-print(market.PostGetPearlItems())
+# print(market.GetPearlItems())
+# print(market.PostGetPearlItems())
 
 
 # Market
 market.GetMarket().SaveToFile("responses/market/get.json")
 market.PostGetMarket().SaveToFile("responses/market/post.json")
-print(market.GetMarket())
-print(market.PostGetMarket())
+# print(market.GetMarket())
+# print(market.PostGetMarket())
